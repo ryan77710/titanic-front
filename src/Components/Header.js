ffa655a95859d89110273a1dbc4c59418d8b1985
ryan77710 <lolliaryan@ryan-ordi.local>
2022-06-03 06:20:51 +0200

    delete function import data

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,11 @@
 import { toast } from "react-toastify";
-import axios from "axios";
 const Header = (props) => {
   const { isLogin, setIsLogin } = props;
 
-  const ok = async () => {
-    await axios.get(`${process.env.REACT_APP_API_URL}ok`);
-  };
   return (
     <header>
       <span className="slide-in-left">Titanic Analytic</span>
-      <button onClick={ok}>ok</button>
+
       {isLogin ? (
         <button
           className="slide-in-right vibrate-1"
